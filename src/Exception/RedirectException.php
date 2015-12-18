6fbb10a67669486a1e7d6aa323ccaf836a0d81b1
bjanish <brian@janish.org>
2015-12-18 15:37:02 -0600

    added redirect exception and save method

---
+++ src/Exception/RedirectException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * RedirectException.php
+ *
+ * LongDescHere
+ *
+ * PHP version 5
+ *
+ * @category  Reliv
+ * @package   vendor\rcm\core\src\Exception
+ * @author    author Brian Janish <bjanish@relivinc.com>
+ * @copyright 2015 Reliv International
+ * @license   License.txt New BSD License
+ * @version   GIT: <git_id>
+ * @link      https://github.com/reliv
+ */
+
+namespace Rcm\Exception;
+
+class RedirectException extends \Exception
+{
+
+}
